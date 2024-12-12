e39efbc3b8556a4d23c1a81d1cfac57ecd8edbf4
lpreaux <contact@lucaspreaux.fr>
2024-12-12 11:38:42 +0100

    build(docker): add Docker setup and initial test implementation
    Introduce Docker setup with a multi-stage Dockerfile and .dockerignore for building and running the application. Configure Docker Compose with services for the app and a MariaDB database. Also, add initial controller and entity classes for testing purposes, and convert application properties to YAML format.

---
+++ src/main/java/scrumtogether/scrumtogetherapi/controller/Test.java
@@ -0,0 +1,13 @@
+package scrumtogether.scrumtogetherapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("test1");
+        return "test";
+    }
+}
---
+++ src/main/java/scrumtogether/scrumtogetherapi/entities/Test.java
@@ -0,0 +1,17 @@
+package scrumtogether.scrumtogetherapi.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "test")
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
