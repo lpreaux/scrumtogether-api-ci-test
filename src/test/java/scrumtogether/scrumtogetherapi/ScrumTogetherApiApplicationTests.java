825cce8d4df1635e4e40aaf8b1ca5b921ff4163c
lpreaux <contact@lucaspreaux.fr>
2024-12-12 10:26:54 +0100

    build: initialize Spring Boot project with core setup
    Set up a new Spring Boot project with essential dependencies, a base application entry point, and initial configurations. Added `.gitignore`, `.gitattributes`, `compose.yaml` for Docker MariaDB service, and Maven Wrapper for build automation.

---
+++ src/test/java/scrumtogether/scrumtogetherapi/ScrumTogetherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package scrumtogether.scrumtogetherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumTogetherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
